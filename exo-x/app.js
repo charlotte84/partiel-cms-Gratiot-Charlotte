c72c75d1ace5035f60c75abd30c2533c0dc58954
charlotte gratiot Fleurant <charlottefleurant17@gmail.com>
2023-04-20 09:55:06 +0200

    j'ai fait exercice 1 du partiel d'ingenierie

---
+++ exo-x/app.js
@@ -0,0 +1,17 @@
+class avion {
+  constructor(marque, modele, longueur, nbplace, reservoir, vitesse){
+    this.marque = marque;
+    this.modele = modele;
+    this.longueur = longueur;
+    this.nbplace = nbPlace;
+    this.reservoir = reservoir;
+    this.vitesse = vitesse;
+  }
+}
+
+
+//Ici, nous définissons une classe nommée "avion".
+//à partir des arguments: marque, modele, longueur, nbplace, reservoir, vitesse
+//Chaque propriété est créée comme une propriété de l'objet en utilisant le mot-clé "this".
+
+
